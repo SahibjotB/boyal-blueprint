35dd287827c1e80d53f67bf42bf6afaf68bf317e
sahibjotb <sahibjot.28@gmail.com>
2025-09-29 08:25:31 -0400

    casing changes for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 import React from "react"; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Calculator from "./pages/Calculator";
 import Knowledge from "./pages/Knowledge";
 import BookACall from "./pages/BookACall";
