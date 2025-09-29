42085460429ed39b75c683334484ad18462cfcab
sahibjotb <sahibjot.28@gmail.com>
2025-09-29 08:13:35 -0400

    changed logo on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logo from "../assets/boyal-blueprint-white.png";
 
 export default function Navbar() {
   return (
     <header className="bg-black shadow-md">
-      <nav className="mx-8 py-6 flex items-center">
+      <nav className="mx-16 py-4 flex items-center">
         <NavLink
           to="/"
           className={({ isActive }) =>
             `text-2xl font-bold ${isActive ? "text-orange-500" : "text-white hover:text-orange-500"}`
           }
         >
-          The Boyal Blueprint
+          <img src={logo} alt="The Boyal Blueprint Logo" className="w-40 h-20 hover:scale-110 ml-8" />
+
         </NavLink>
-        <ul className="ml-auto flex space-x-6 text-sm sm:text-base font-medium">
+        <ul className="ml-auto flex space-x-6 text-xl font-semibold">
           <li>
             <NavLink
               to="/calculator"
