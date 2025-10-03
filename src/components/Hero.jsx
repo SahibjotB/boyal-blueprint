334ed448eb37b1ce01ac993fb4ec601d550d1820
sahibjotb <sahib_boyal@hotmail.com>
2025-10-03 19:39:39 -0400

    Fix mobile wave flicker

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import WaveDivider from "./WaveDivider";
 
 export default function Hero() {
   return (
-    <section id='hero' className="relative w-full min-h-screen flex flex-col justify-start pt-44 items-center text-center px-4 sm:px-8 overflow-hidden">
+    <section id='hero' className="relative w-full min-h-screen flex flex-col justify-start pt-36 md:pt-44 items-center text-center px-4 sm:px-8 overflow-hidden">
       <h1 className="text-3xl sm:text-3xl md:text-5xl text-gray-900 dark:text-white font-display font-medium">
         Welcome to
       </h1>
